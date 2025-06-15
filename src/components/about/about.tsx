0f5ce2f8437f789d75ab24eab2ab7f45b8cc26e7
Grigoryan <grigoryanelmira01@gmail.com>
2025-06-15 22:48:12 +0400

    Create About page

---
+++ src/components/about/about.tsx
@@ -0,0 +1,64 @@
+const AboutUsPage=()=>{
+    return(
+    <div className="flex flex-col items-start gap-2">
+        <div className="">
+            <h1 className='text-2xl lg:text-4xl font-bold'></h1>
+            <p></p>
+        </div>
+          <div className="">
+            <h1 className='text-2xl lg:text-4xl font-bold'></h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+          <div className="">
+            <h1 className='text-2xl lg:text-4xl font-bold'></h1>
+            <p></p>
+                <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+          <div className="">
+            <h1 className='text-2xl lg:text-4xl font-bold'></h1>
+            <p></p>
+                <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+           <div className="">
+            <h1 className='text-2xl lg:text-4xl font-bold'></h1>
+                <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        
+           <div className="">
+            <h1 className='text-2xl lg:text-4xl font-bold'></h1>
+                <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        
+           <div className="">
+            <h1 className='text-2xl lg:text-4xl font-bold'>Присоединяйтесь к нам!</h1>
+             <p>Будь вы новичок, преподаватель или профи — добро пожаловать! Вместе мы создаём платформу, где Python учат с интересом.</p>
+        </div>
+    </div>
+    )
+}
+
+export default AboutUsPage
