caae51ae5869c2874c83624a9b500ee941558010
Grigoryan <grigoryanelmira01@gmail.com>
2025-06-16 23:36:11 +0400

    Create Progress page

---
+++ src/app/dashboard/tests/page.tsx
@@ -0,0 +1,10 @@
+import TestPage from "@/components/tests/test"
+
+const Tests=()=>{
+    return(
+        <TestPage />
+    )
+}
+
+
+export default Tests
---
+++ src/components/tests/test.tsx
@@ -0,0 +1,13 @@
+import Sidebar from '../../components/tests/sidebar';
+import QuizPage from '../../components/tests/quiz'
+
+const TestPage=()=>{
+    return(
+        <>
+        <Sidebar />
+        <QuizPage />
+        </>
+    )
+}
+
+export default TestPage
